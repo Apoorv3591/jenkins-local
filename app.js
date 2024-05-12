f4f0742f0df14ea7ecf4e7523012ea6869559b0e
apoorv3591 <apoorv3591@gmail.com>
2024-05-12 22:51:26 +0530

    41

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route to handle GET requests to the root URL '/'
 app.get('/', (req, res) => {
-    res.send('Hello, World! 40');
+    res.send('Hello, World! 41');
 });
 
 // Start the server and listen on port 3000
